648b433ef90f5cdc22ad3293aeb537eb0ba6e9dc
Greg MacWilliam <greg.macwilliam@voxmedia.com>
2014-09-04 16:31:04 -0400

    Adding SBN community logos.

--- source/javascripts/settings.js
+++ source/javascripts/settings.js
@@ -1,10 +1,13 @@
+var watermark = document.host.replace(/.*?\.?(.+)\..*?/, '$1');
+watermark = 'bloodyelbow';
+
 var MEME_SETTINGS = {
   creditText: 'Source:', // Default "credits" text.
   creditSize: 12, // Font size for credit text.
   fontColor: 'white', // Universal font color.
-  fontFamily: 'Helvetica Neue', // Default font family selection.
+  fontFamily: 'Helvetica', // Default font family selection.
   // Font family options: set to empty array to disable font selector.
-  fontFamilyOpts: ['Helvetica', 'Helvetica Neue', 'Comic Sans MS'],
+  fontFamilyOpts: [],
   fontSize: 24, // Font size of main headline.
   // Font size options: set to empty array to disable font-size selector.
   fontSizeOpts: [14, 24, 36],
@@ -15,7 +18,7 @@ var MEME_SETTINGS = {
   overlayAlpha: 0.5, // Opacity of image overlay.
   overlayColor: '#000', // Image overlay color, or blank for no overlay.
   // Overlay color options: set to empty array to disable overlay options selector.
-  overlayColorOpts: ['#000', '#777', '#2980b9'],
+  overlayColorOpts: ['#000', '#777', '#d32027'],
   paddingRatio: 0.05, // Percentage of canvas width to use as edge padding.
   textAlign: 'left', // Text alignment.
   // Text alignment options: set to empty array to disable alignment picker.
@@ -24,6 +27,6 @@ var MEME_SETTINGS = {
   textShadowEdit: true, // Toggles text shadow control within the editor.
   watermarkAlpha: 0.75, // Opacity of watermark image.
   watermarkMaxWidthRatio: 0.20, // Maximum allowed width of watermark, relative to canvas width.
-  watermarkSrc: 'images/verge-logo.png', // Watermark image path. MUST reside on host domain, or use base64 data.
+  watermarkSrc: 'images/'+watermark+'.png', // Watermark image path. MUST reside on host domain, or use base64 data.
   width: 755 // Canvas rendering width.
 };
