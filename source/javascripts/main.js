5fbe8aed3e40374ef0d141d6e9afe9a9b8d18c99
Greg MacWilliam <greg.macwilliam@voxmedia.com>
2014-09-05 10:46:24 -0400

    Adding watermark switching control.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,55 +1,47 @@
-function waitForWebfonts(fonts, callback) {
+function waitForFonts(fonts, callback) {
     var loadedFonts = 0;
-    for(var i = 0, l = fonts.length; i < l; ++i) {
-        (function(font) {
-            var node = document.createElement('span');
-            // Characters that vary significantly among different fonts
-            node.innerHTML = 'giItT1WQy@!-/#';
-            // Visible - so we can measure it - but not on the screen
-            node.style.position      = 'absolute';
-            node.style.left          = '-10000px';
-            node.style.top           = '-10000px';
-            // Large font size makes even subtle changes obvious
-            node.style.fontSize      = '300px';
-            // Reset any font properties
-            node.style.fontFamily    = 'sans-serif';
-            node.style.fontVariant   = 'normal';
-            node.style.fontStyle     = 'normal';
-            node.style.fontWeight    = 'normal';
-            node.style.letterSpacing = '0';
-            document.body.appendChild(node);
+    var pendingFonts = [];
 
-            // Remember width with no applied web font
-            var width = node.offsetWidth;
+    function PendingFont(font) {
+        var el = document.createElement('span');
+        el.innerHTML = 'giItT1WQy@!-/#';
+        el.style.position = 'absolute';
+        el.style.left = el.style.top = '-10000px';
+        el.style.visibility = 'hidden';
+        el.style.fontSize = '300px';
+        el.style.fontFamily = 'sans-serif';
+        el.style.fontVariant = 'normal';
+        el.style.fontStyle = 'normal';
+        el.style.fontWeight = 'normal';
+        el.style.letterSpacing = '0';
+        document.body.appendChild(el);
 
-            node.style.fontFamily = font;
-
-            var interval;
-            function checkFont() {
-                // Compare current width with original width
-                if(node && node.offsetWidth != width) {
-                    ++loadedFonts;
-                    node.parentNode.removeChild(node);
-                    node = null;
-                }
-
-                // If all fonts have been loaded
-                if(loadedFonts >= fonts.length) {
-                    if(interval) {
-                        clearInterval(interval);
-                    }
-                    if(loadedFonts == fonts.length) {
-                        callback();
-                        return true;
-                    }
-                }
-            };
-
-            if(!checkFont()) {
-                interval = setInterval(checkFont, 50);
-            }
-        })(fonts[i]);
+        this.el = el;
+        this.sw = el.offsetWidth;
+        el.style.fontFamily = font;
     }
+
+    function testFonts() {
+        for (var i=0; i < pendingFonts.length; ++i) {
+            var pending = pendingFonts[i];
+            if (pending.el && pending.el.offsetWidth != pending.sw) {
+                pending.el.parentNode.removeChild(pending.el);
+                pending.el = null;
+                loadedFonts++;
+            }
+        }
+
+        if (loadedFonts < pendingFonts.length) {
+            setTimeout(testFonts, 50);
+        } else if (typeof callback == 'function') {
+            callback();
+        }
+    }
+
+    for (var i=0; i < fonts.length; i++) {
+        pendingFonts.push(new PendingFont(fonts[i]));
+    }
+    testFonts();
 }
 
 MEME = {
@@ -75,10 +67,9 @@ MEME = {
     // Parse out fonts list:
     var fontsList = this.model.get('fontFamily').split(',');
     fontsList = fontsList.concat(this.model.get('fontFamilyOpts') || []);
-    console.log(fontsList);
 
     // Re-render view after all fonts load:
-    waitForWebfonts(fontsList, function() {
+    waitForFonts(fontsList, function() {
       MEME.render();
     });
   }
