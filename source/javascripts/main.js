7ace4526a42105aacab8b1671f5b6be295a3455c
Greg MacWilliam <greg.macwilliam@voxmedia.com>
2014-09-05 13:46:09 -0400

    Adding font-monitor helper.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,50 +1,6 @@
-function waitForFonts(fonts, callback) {
-    var loadedFonts = 0;
-    var pendingFonts = [];
-
-    function PendingFont(font) {
-        var el = document.createElement('span');
-        el.innerHTML = 'giItT1WQy@!-/#';
-        el.style.position = 'absolute';
-        el.style.left = el.style.top = '-10000px';
-        el.style.visibility = 'hidden';
-        el.style.fontSize = '300px';
-        el.style.fontFamily = 'sans-serif';
-        el.style.fontVariant = 'normal';
-        el.style.fontStyle = 'normal';
-        el.style.fontWeight = 'normal';
-        el.style.letterSpacing = '0';
-        document.body.appendChild(el);
-
-        this.el = el;
-        this.sw = el.offsetWidth;
-        el.style.fontFamily = font;
-    }
-
-    function testFonts() {
-        for (var i=0; i < pendingFonts.length; ++i) {
-            var pending = pendingFonts[i];
-            if (pending.el && pending.el.offsetWidth != pending.sw) {
-                pending.el.parentNode.removeChild(pending.el);
-                pending.el = null;
-                loadedFonts++;
-            }
-        }
-
-        if (loadedFonts < pendingFonts.length) {
-            setTimeout(testFonts, 50);
-        } else if (typeof callback == 'function') {
-            callback();
-        }
-    }
-
-    for (var i=0; i < fonts.length; i++) {
-        pendingFonts.push(new PendingFont(fonts[i]));
-    }
-    testFonts();
-}
-
 MEME = {
+  $: jQuery,
+
   render: function() {
     this.canvas && this.canvas.render();
   },
@@ -64,17 +20,13 @@ MEME = {
       model: this.model
     });
 
-    // Parse out fonts list:
-    var fontsList = this.model.get('fontFamily').split(',');
-    fontsList = fontsList.concat(this.model.get('fontFamilyOpts') || []);
-
     // Re-render view after all fonts load:
-    waitForFonts(fontsList, function() {
+    this.waitForFonts().then(function() {
       MEME.render();
     });
   }
 };
 
-jQuery(function() {
+MEME.$(function() {
   MEME.init();
 });
