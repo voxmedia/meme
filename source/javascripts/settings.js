ac19c95aba785f43f6293a234eb01b72842a8cd0
Andrew Chavez <adchavez@gmail.com>
2014-12-08 16:51:21 -0600

    Make no-overlay the default

--- source/javascripts/settings.js
+++ source/javascripts/settings.js
@@ -27,7 +27,7 @@ var MEME_SETTINGS = {
   overlayAlpha: 0.5, // Opacity of image overlay.
 
   // Image overlay color, or blank ('') for no overlay:
-  overlayColor: '#000',
+  overlayColor: '',
   // Overlay color options: set to empty array to disable overlay options selector.
   overlayColorOpts: ['#000', '#777', '#1368b2', '#98012e', '#62b246', '#1955a5', '#BF5700'],
   paddingRatio: 0.05, // Percentage of canvas width to use as edge padding.
