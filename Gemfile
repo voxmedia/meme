36585db0e28a8c6c31763fbc98e8c05efcab3aea
Juan Farias <juan_farias@artear.com>
2015-04-16 15:55:58 -0300

    some deploy things

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source 'http://rubygems.org'
 
 gem "middleman", "~>3.3.3"
+gem "middleman-minify-html"
 
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
--- config.rb
+++ config.rb
@@ -29,10 +29,12 @@ set :absolute_prefix, 'http://localhost:4567'
 # Build-specific configuration
 configure :build do
   puts "local build"
-  set :url_prefix, ""
-  set :absolute_prefix, ""
+  set :url_prefix, "prefix/"
+  set :absolute_prefix, "./"
+  set :images_dir, 'images'
     
   activate :asset_hash
+  activate :minify_html
   activate :minify_javascript
   activate :minify_css
   activate :relative_assets
