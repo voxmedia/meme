f59c054a0e4b0720efefcce93f2a17990496e368
Juan Alberto Farias <dientuki@gmail.com>
2015-04-17 17:28:26 -0300

    fix download

--- source/javascripts/views/meme-canvas.js
+++ source/javascripts/views/meme-canvas.js
@@ -255,7 +255,7 @@ MEME.MemeCanvasView = Backbone.View.extend({
     renderRibbon(ctx);
     renderEmoji(ctx);
     
-    this.model.data = this.canvas.toDataURL("image/jpeg", 0.8); //.replace('image/png', 'image/octet-stream');
+    this.model.data = this.canvas.toDataURL("image/jpeg", 1); //.replace('image/png', 'image/octet-stream');
     // Enable drag cursor while canvas has artwork:
     this.canvas.style.cursor = this.model.background.width ? 'move' : 'default';
   },
