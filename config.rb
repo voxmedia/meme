9b916fde6df048a8ba2e343b402330f22e981d08
J. Heasly <jheasly@earthlink.net>
2015-06-26 18:42:23 -0700

    Figured out the path thing.

--- config.rb
+++ config.rb
@@ -30,7 +30,8 @@ set :absolute_prefix, 'http://localhost:4567'
 configure :build do
   puts "local build"
   set :url_prefix, ""
-#   set :absolute_prefix, ""
+  set :absolute_prefix, ""
+  activate :relative_assets
   activate :asset_hash
   activate :minify_javascript
   activate :minify_css
