973c21b9b6bdbebdc394e0d90df636d5f908289f
Ezra Mechaber <ezra.mechaber@gmail.com>
2017-06-24 20:41:16 -0400

    Not pulling from settings correctly
    Either a result of meme.js, partials, or maybe my settings file?

--- source/javascripts/models/meme.js
+++ source/javascripts/models/meme.js
@@ -4,32 +4,32 @@
 */
 MEME.MemeModel = Backbone.Model.extend({
   defaults: {
-    backgroundPosition: { x: null, y: null },
-    creditText: "Source:",
-    creditSize: 12,
-    downloadName: "share",
-    fontColor: "white",
-    fontFamily: "Helvetica Neue",
-    fontFamilyOpts: ["Helvetica", "Helvetica Neue", "Comic Sans MS"],
-    fontSize: 24,
-    fontSizeOpts: [14, 24, 36],
-    headlineText: "Write your own headline",
-    height: 378,
-    imageScale: 1,
-    imageSrc: "",
-    overlayAlpha: 0.5,
-    overlayColor: "#000",
-    overlayColorOpts: ["#000", "#777", "#2980b9"],
     backgroundColor: null,
     backgroundColorOpts: ["#FFF", "#292929", "#CCC", "#7AB8CF", "#FFF200"],
+    backgroundPosition: { x: null, y: null },
+    creditText: 'Source:',
+    creditSize: 12,
+    downloadName: 'share',
+    fontColor: 'white',
+    fontFamily: 'Helvetica Neue',
+    fontFamilyOpts: ['Helvetica', 'Helvetica Neue', 'Comic Sans MS'],
+    fontSize: 24,
+    fontSizeOpts: [14, 24, 36],
+    headlineText: 'Write your own headline',
+    height: 378,
+    imageScale: 1,
+    imageSrc: '',
+    overlayAlpha: 0.5,
+    overlayColor: '#000',
+    overlayColorOpts: ['#000', '#777', '#2980b9'],
     paddingRatio: 0.05,
-    textAlign: "left",
-    textAlignOpts: ["left", "center", "right"],
+    textAlign: 'left',
+    textAlignOpts: ['left', 'center', 'right'],
     textShadow: true,
     textShadowEdit: true,
     watermarkAlpha: 0.75,
     watermarkMaxWidthRatio: 0.25,
-    watermarkSrc: "",
+    watermarkSrc: '',
     watermarkOpts: [],
     width: 755
   },
@@ -42,20 +42,19 @@ MEME.MemeModel = Backbone.Model.extend({
 
     // Set image sources to trigger "change" whenever they reload:
     this.background.onload = this.watermark.onload = _.bind(function() {
-      this.trigger("change");
+      this.trigger('change');
     }, this);
 
     // Set initial image and watermark sources:
-    if (this.get("imageSrc")) this.background.src = this.get("imageSrc");
-    if (this.get("watermarkSrc"))
-      this.setWatermarkSrc(this.get("watermarkSrc"));
+    if (this.get('imageSrc')) this.background.src = this.get('imageSrc');
+    if (this.get('watermarkSrc')) this.setWatermarkSrc(this.get('watermarkSrc'));
 
     // Update image and watermark sources if new source URLs are set:
-    this.listenTo(this, "change:imageSrc", function() {
-      this.background.src = this.get("imageSrc");
+    this.listenTo(this, 'change:imageSrc', function() {
+      this.background.src = this.get('imageSrc');
     });
-    this.listenTo(this, "change:watermarkSrc", function() {
-      this.setWatermarkSrc(this.get("watermarkSrc"));
+    this.listenTo(this, 'change:watermarkSrc', function() {
+      this.setWatermarkSrc(this.get('watermarkSrc'));
     });
   },
 
@@ -67,9 +66,7 @@ MEME.MemeModel = Backbone.Model.extend({
   // Loads a file stream into an image object:
   loadFileForImage: function(file, image) {
     var reader = new FileReader();
-    reader.onload = function() {
-      image.src = reader.result;
-    };
+    reader.onload = function() { image.src = reader.result; };
     reader.readAsDataURL(file);
   },
 
@@ -88,17 +85,17 @@ MEME.MemeModel = Backbone.Model.extend({
   // The option's "data" attribute will be set as the watermark, if defined.
   // This is useful for avoiding cross-origin resource loading issues.
   setWatermarkSrc: function(src) {
-    var opt = _.findWhere(this.get("watermarkOpts"), { value: src });
+    var opt = _.findWhere(this.get('watermarkOpts'), {value: src});
     var data = (opt && opt.data) || src;
 
     // Toggle cross-origin attribute for Data URI requests:
-    if (data.indexOf("data:") === 0) {
-      this.watermark.removeAttribute("crossorigin");
+    if (data.indexOf('data:') === 0) {
+      this.watermark.removeAttribute('crossorigin');
     } else {
-      this.watermark.setAttribute("crossorigin", "anonymous");
+      this.watermark.setAttribute('crossorigin', 'anonymous');
     }
 
     this.watermark.src = data;
-    this.set("watermarkSrc", src);
+    this.set('watermarkSrc', src);
   }
 });
